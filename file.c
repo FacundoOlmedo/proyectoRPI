7aaeb3af2f994b4249798d2af742dcc84f920fb1
FacundoOlmedo <facundo.olmedo13@gmail.com>
2022-01-07 22:19:22 -0300

    First commit

---
+++ file.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+
+        printf("Hola Mundo wachin bien ahi\n\nEsta es la mejor forma de conectar la raspberrylocur\n\na");
+
+return 0;
+}
